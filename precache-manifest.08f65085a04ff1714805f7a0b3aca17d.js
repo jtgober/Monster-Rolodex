fe6fc7ce759941e2143e2869b5a856b8ac3d8722
Jonathan Gober <gober.jonathan@gmail.com>
2020-04-08 19:10:59 -0400

    Updates

---
+++ precache-manifest.08f65085a04ff1714805f7a0b3aca17d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "be91ac53f2f40245e33f0c0913972468",
+    "url": "/Monster-Rolodex/index.html"
+  },
+  {
+    "revision": "3f2a217ba8b72080baa2",
+    "url": "/Monster-Rolodex/static/css/main.f4ae7d44.chunk.css"
+  },
+  {
+    "revision": "147f1327e7798733efc4",
+    "url": "/Monster-Rolodex/static/js/2.d562cfe4.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Monster-Rolodex/static/js/2.d562cfe4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3f2a217ba8b72080baa2",
+    "url": "/Monster-Rolodex/static/js/main.d661f655.chunk.js"
+  },
+  {
+    "revision": "5aae74e9f2efb66f6413",
+    "url": "/Monster-Rolodex/static/js/runtime-main.609a1191.js"
+  }
+]);
